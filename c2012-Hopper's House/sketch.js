18fdd67e7eee46fd195a12dab065089b63c14d58
Lloyd Napata <29262019+loynaps@users.noreply.github.com>
2019-02-20 13:38:24 +0200

    using triangle()

---
+++ c2012-Hopper's House/sketch.js
@@ -0,0 +1,32 @@
+/*
+201 - The case of Judge Hopper
+Stage 2 - Hopper’s House
+
+Officer: 6386874
+CaseNum: 201-1-84132140-6386874
+
+On entering the house you’re immediately confronted by a trail of blood spatters leading up to the master bedroom. It’s clear that Judge Hopper was abducted from here. What’s more, the clues splayed across the bed sheets indicate a private life a little more exciting than a district judge would admit in public. Somehow these clues connect, you just need to join them together.
+
+Use the triangle function to join the blood spatters on the clues and make the connection.
+
+*/
+
+var img;
+
+function preload() {
+  img = loadImage("scene.png");
+}
+
+function setup() {
+  createCanvas(img.width, img.height);
+}
+
+function draw() {
+  image(img, 0, 0);
+  stroke(255, 0, 0);
+  strokeWeight(3);
+  noFill();
+
+  //splatter points
+  triangle(146, 373, 398, 209, 983, 334);
+}
